719e49b53cdcc81bcd98e262a4887f0865b9a034
jiansen <1096129293@qq.com>
2025-09-10 11:08:19 +0800

    fix: 设置存储

--- routes/api/v1/[id].js
+++ routes/api/v1/[id].js
@@ -2,8 +2,14 @@ import { createDefine } from "fresh";
 
 const app = createDefine();
 export const handler = app.handlers({
-  GET(ctx) {
+  async GET(ctx) {
     const name = ctx.params.id;
+
+    const kv = await Deno.openKv();
+    await kv.set(["name"], name, { expireIn: 60_000 });
+
+    const kvValue = await kv.get(["name"]);
+    console.log(kvValue.value);
     return new Response(
       `Hello, ${name}!`,
     );
